5988c4ad6fe4f0f8bd332b027de49ca41bb0eb15
stevenkearns <sjk336@cs.drexel.edu>
2020-08-21 19:06:39 -0400

    Overhaul frontend, install vuetify and vue-router, get basic homepage set up.

---
+++ frontend/src/router.js
@@ -0,0 +1,15 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import MainMenu from "@/components/MainMenu";
+
+Vue.use(VueRouter);
+export default new VueRouter({
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      name: "MainMenu",
+      component: MainMenu,
+    },
+  ],
+});
