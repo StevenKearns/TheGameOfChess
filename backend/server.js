9d25d2d61d7e5fd193b5f641a15c08589b24cd6c
Suin Kim <suinkim35@gmail.com>
2020-08-24 20:51:25 -0400

    Updated backend

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,26 @@ const Express = require("express");
 const Http = require("http").Server(Express);
 const Socketio = require("socket.io")(Http);
 
-/*
 var chessboard = {
-
+    colors: ["white", "brown"],
+    // Very proud of the colors
+    squares: Array(8 * 8)
+        .fill()
+        .map(() => ""),
+    firstSpot: null,
 };
-*/
 
-/*
-Socketio.onconnection("connection", socket => {
+Socketio.on("connection", socket => {
+    console.log("Player has connected.");
     socket.emit("chessboard", chessboard);
-})
-*/
+    console.log(chessboard);
+    socket.on("move", data => {
+        console.log(data);
+        chessboard.squares = data;
+        Socketio.emit("chessboard", chessboard);
+    });
+});
 
 Http.listen(3000, () => {
     console.log("Listening at :3000...");
-})
+});
